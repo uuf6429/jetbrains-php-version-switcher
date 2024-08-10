561b38c1d384f0794902d6518d7bc1e0b81baa18
Christian Sciberras <christian@sciberras.me>
2024-08-10 23:17:24 +0200

    fix persisting settings

--- src/main/kotlin/me/sciberras/christian/pvs/ProjectSettings.kt
+++ src/main/kotlin/me/sciberras/christian/pvs/ProjectSettings.kt
@@ -4,13 +4,13 @@ import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 
 @Service(Service.Level.PROJECT)
-@State(name = "PVS.Settings", storages = [Storage(StoragePathMacros.PRODUCT_WORKSPACE_FILE)])
+@State(name = "PhpVersionSwitcher", storages = [Storage("pvs.xml")])
 class ProjectSettings(
     private val project: Project,
 ) : SimplePersistentStateComponent<ProjectSettingsState>(ProjectSettingsState())
 
 class ProjectSettingsState : BaseState() {
-    var enabled: TriState = TriState.UNDEFINED
+    var enabled: TriState by enum<TriState>(TriState.UNDEFINED)
 }
 
 enum class TriState {
