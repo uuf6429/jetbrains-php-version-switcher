596f6b75416846a98fda7b195e85fef4c2b10c08
Christian Sciberras <christian@sciberras.me>
2024-08-10 22:57:20 +0200

    remove (broken) binding approach

--- src/main/kotlin/me/sciberras/christian/pvs/ProjectSettingsConfigurable.kt
+++ src/main/kotlin/me/sciberras/christian/pvs/ProjectSettingsConfigurable.kt
@@ -3,18 +3,17 @@ package me.sciberras.christian.pvs
 import com.intellij.openapi.components.service
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
-import com.intellij.ui.dsl.builder.bindItem
+import com.intellij.openapi.ui.ComboBox
+import com.intellij.ui.dsl.builder.Cell
 import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.dsl.builder.toNullableProperty
 import com.intellij.ui.dsl.listCellRenderer.textListCellRenderer
 import javax.swing.JComponent
 
 class ProjectSettingsConfigurable(
-    private val project: Project
+    project: Project
 ) : Configurable {
     private val stateService = project.service<ProjectSettings>()
-    private var initialState = this.stateService.state
-    private var currentState = ProjectSettingsState()
+    private var enabledField: Cell<ComboBox<TriState>>? = null
 
     override fun getDisplayName(): String {
         return "My Project Settings 2" // TODO this seems to be unused
@@ -23,7 +22,7 @@ class ProjectSettingsConfigurable(
     override fun createComponent(): JComponent {
         return panel {
             row("Refresh language level") {
-                comboBox(
+                enabledField = comboBox(
                     TriState.entries,
                     textListCellRenderer {
                         when (it) {
@@ -33,24 +32,22 @@ class ProjectSettingsConfigurable(
                             else -> ""
                         }
                     }
-                ).bindItem(currentState::enabled.toNullableProperty())
+                )
             }
         }
     }
 
     override fun isModified(): Boolean {
-        // TODO forced to true, so that the "Apply" button is clickable so we can see trigger the method below
-        return true; //this.currentState != this.initialState
+        return this.enabledField!!.component.selectedItem as TriState? != stateService.state.enabled
     }
 
     override fun apply() {
-        // FIXME at this point, this.currentState.enabled should be what the user has selected, but it is never so
-        this.stateService.loadState(this.currentState)
-        this.initialState.copyFrom(this.currentState)
+        stateService.state.enabled = this.enabledField!!.component.selectedItem as TriState? ?: TriState.UNDEFINED
     }
 
     override fun reset() {
-        this.currentState.copyFrom(this.initialState)
+        this.enabledField!!.component.selectedItem = stateService.state.enabled
+
         super.reset()
     }
 }
