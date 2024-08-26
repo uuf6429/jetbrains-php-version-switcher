7add868bc4bd7964acbeb48482d5dddaa5b19a40
Christian Sciberras <christian@sciberras.me>
2024-08-26 23:26:25 +0200

    Improve search for file in parent directories; add debug logging

--- src/main/kotlin/me/sciberras/christian/pvs/Extensions.kt
+++ src/main/kotlin/me/sciberras/christian/pvs/Extensions.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.findPsiFile
+import com.intellij.openapi.vfs.isFile
 import com.jetbrains.php.config.PhpLanguageLevel
 import com.jetbrains.php.config.composer.LanguageLevelComposerParser
 import com.jetbrains.php.lang.inspections.PhpSwitchComposerLanguageLevelQuickFix
@@ -42,4 +43,18 @@ internal fun VirtualFile.findComposerPhpProperty(project: Project): String? {
     val psiFile = this.findPsiFile(project)
     val jsonProp = PhpSwitchComposerLanguageLevelQuickFix.findPhpProperty(psiFile)?.value as? JsonStringLiteral
     return jsonProp?.value
+}
+
+fun VirtualFile.findNearestFile(fileName: String): VirtualFile? {
+    var directory = this.parent
+
+    while (directory != null) {
+        val targetFile = directory.findChild(fileName)
+        if (targetFile?.isFile == true) {
+            return targetFile
+        }
+        directory = directory.parent
+    }
+
+    return null
 }
--- src/main/kotlin/me/sciberras/christian/pvs/FocusChangeListener.kt
+++ src/main/kotlin/me/sciberras/christian/pvs/FocusChangeListener.kt
@@ -1,6 +1,7 @@
 package me.sciberras.christian.pvs
 
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.FocusChangeListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
@@ -28,14 +29,16 @@ internal class FocusChangeListener : FocusChangeListener {
         }
         lastPhpFile = phpFile
 
-        val composerFile = detectNearestComposeFile(phpFile)
+        val composerFile = phpFile.findNearestFile("composer.json")
         if (composerFile == null || composerFile == lastComposerFile) {
+            thisLogger().debug("Composer file not found in any parent directory")
             return
         }
         lastComposerFile = composerFile
 
         val phpVersion = composerFile.findPhpVersion(project)
         if (phpVersion == null || phpVersion == lastPhpVersion) {
+            thisLogger().debug("PHP not set as a requirement in $composerFile")
             return
         }
         lastPhpVersion = phpVersion
@@ -43,18 +46,8 @@ internal class FocusChangeListener : FocusChangeListener {
         setPhpVersion(project, phpVersion)
     }
 
-    private fun detectNearestComposeFile(phpFile: VirtualFile): VirtualFile? {
-        val segments = phpFile.path.split('/').count();
-        for (i in 0..segments) {
-            val composerFile = phpFile.findFileByRelativePath("../".repeat(i) + "composer.json")
-            if (composerFile !== null) {
-                return composerFile
-            }
-        }
-        return null
-    }
-
     private fun setPhpVersion(project: Project, phpVersion: PhpLanguageLevel) {
+        thisLogger().debug("Setting PHP version to $phpVersion...")
         PhpProjectConfigurationFacade.getInstance(project).languageLevel = phpVersion
     }
 
