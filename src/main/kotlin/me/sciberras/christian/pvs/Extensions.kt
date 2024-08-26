06227a9f713d5980af2e7223dd6828524698b2e7
Christian Sciberras <christian@sciberras.me>
2024-08-27 00:31:57 +0200

    Improve and fix composer file detection (#13)
    * Improve search for file in parent directories; add debug logging
    
    * Fix previous composer-file/php-version conflict
    
    * Use JetBrains-defined default for default language level
    
    * Improve handling of missing PHP requirement in composer files

--- src/main/kotlin/me/sciberras/christian/pvs/Extensions.kt
+++ src/main/kotlin/me/sciberras/christian/pvs/Extensions.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.findPsiFile
+import com.intellij.openapi.vfs.isFile
 import com.jetbrains.php.config.PhpLanguageLevel
 import com.jetbrains.php.config.composer.LanguageLevelComposerParser
 import com.jetbrains.php.lang.inspections.PhpSwitchComposerLanguageLevelQuickFix
@@ -15,11 +16,12 @@ fun VirtualFile.findPhpVersion(project: Project): PhpLanguageLevel? {
     try {
         return when {
             this.name == "composer.json" -> {
-                LanguageLevelComposerParser
-                    .getMinRequiredLanguageLevel(
-                        this.findComposerPhpProperty(project),
-                        PhpLanguageLevel.PHP820
-                    )
+                this.findComposerPhpProperty(project)
+                    ?.takeIf { it.isNotBlank() }
+                    ?.run {
+                        LanguageLevelComposerParser
+                            .getMinRequiredLanguageLevel(this, PhpLanguageLevel.DEFAULT)
+                    }
             }
 
             this.name == "composer.lock" -> {
@@ -42,4 +44,18 @@ internal fun VirtualFile.findComposerPhpProperty(project: Project): String? {
     val psiFile = this.findPsiFile(project)
     val jsonProp = PhpSwitchComposerLanguageLevelQuickFix.findPhpProperty(psiFile)?.value as? JsonStringLiteral
     return jsonProp?.value
+}
+
+fun VirtualFile.findNearestFile(fileName: String): VirtualFile? {
+    var directory = this.parent
+
+    while (directory != null) {
+        val targetFile = directory.findChild(fileName)
+        if (targetFile?.isFile == true) {
+            return targetFile
+        }
+        directory = directory.parent
+    }
+
+    return null
 }
--- src/main/kotlin/me/sciberras/christian/pvs/FocusChangeListener.kt
+++ src/main/kotlin/me/sciberras/christian/pvs/FocusChangeListener.kt
@@ -1,6 +1,7 @@
 package me.sciberras.christian.pvs
 
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.FocusChangeListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
@@ -28,33 +29,35 @@ internal class FocusChangeListener : FocusChangeListener {
         }
         lastPhpFile = phpFile
 
-        val composerFile = detectNearestComposeFile(phpFile)
-        if (composerFile == null || composerFile == lastComposerFile) {
+        val composerFile = phpFile.findNearestFile("composer.json")
+        if (composerFile == lastComposerFile) {
             return
         }
+
         lastComposerFile = composerFile
 
-        val phpVersion = composerFile.findPhpVersion(project)
-        if (phpVersion == null || phpVersion == lastPhpVersion) {
+        if (composerFile == null) {
+            thisLogger().debug("Composer file not found in any parent directory of $phpFile")
             return
         }
+
+        val phpVersion = composerFile.findPhpVersion(project)
+        if (phpVersion == lastPhpVersion) {
+            return
+        }
+
         lastPhpVersion = phpVersion
+
+        if (phpVersion == null) {
+            thisLogger().debug("PHP not set as a requirement in $composerFile")
+            return
+        }
 
         setPhpVersion(project, phpVersion)
     }
 
-    private fun detectNearestComposeFile(phpFile: VirtualFile): VirtualFile? {
-        val segments = phpFile.path.split('/').count();
-        for (i in 0..segments) {
-            val composerFile = phpFile.findFileByRelativePath("../".repeat(i) + "composer.json")
-            if (composerFile !== null) {
-                return composerFile
-            }
-        }
-        return null
-    }
-
     private fun setPhpVersion(project: Project, phpVersion: PhpLanguageLevel) {
+        thisLogger().debug("Setting PHP version to $phpVersion...")
         PhpProjectConfigurationFacade.getInstance(project).languageLevel = phpVersion
     }
 
