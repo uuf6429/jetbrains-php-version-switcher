16cd57a5cd7892ed6024a959f83eb82d3cc1ae69
Christian Sciberras <christian@sciberras.me>
2024-09-29 16:25:20 +0200

    Update for 2024.3 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+group = "me.sciberras.christian"
+version = "1.0.4"
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("org.jetbrains.intellij.platform") version "2.0.0"
 }
-
-group = "me.sciberras.christian"
-version = "1.0.3"
 
 repositories {
     mavenCentral()
@@ -24,6 +24,8 @@ val ideVersion = System.getenv("IDE_VERSION") ?: "2023.2"
 val pluginVersion = mapOf(
     "2023" to "232.8660.185",
     "2024" to "242.20224.427",
+    "242" to "242.20224.427",
+    "243" to "243.16718.32",
 )[ideVersion.split('.').first()]
 
 dependencies {
@@ -65,7 +67,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("243.*")
     }
 
     signPlugin {
